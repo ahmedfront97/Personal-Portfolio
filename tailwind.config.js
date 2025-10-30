e7c62c83759a0866f7bb95e42adf4b17c87cc160
Ahmed Abbas <ahmedfront97>
2025-10-30 16:35:47 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// module.exports = {
+//   content: ["./src/**/*.{html,js}", "./*.{html,js}"],
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         primary: ["Doto", "sans-serif"],
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
